93f2757c062ae83f22dfc8e5fe45ea53d74440c5
Md Abir Hossain <hossain.mdabir@gmail.com>
2024-08-10 15:49:40 +0600

    Files added.

---
+++ Quiz App/ViewModel/QuizViewModel.swift
@@ -0,0 +1,16 @@
+//
+// 
+// FileName: QuizViewModel.swift
+// ProjectName: Quiz App
+//
+// Created by MD ABIR HOSSAIN on 10-08-2024 at 3:49 PM
+// Website: https://mdabirhossain.com/
+//
+
+
+import Foundation
+
+
+class QuizViewModel: ObservableObject {
+    
+}
