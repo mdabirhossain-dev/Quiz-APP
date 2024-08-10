7bb5b7287d28fa1bfafe62c41cd14fb37991bcea
Md Abir Hossain <mdabirhossain.dev@gmail.com>
2024-08-11 02:23:28 +0600

    Extension to check if not empty

---
+++ Quiz App/Extension/Collection.swift
@@ -0,0 +1,17 @@
+//
+// 
+// FileName: Collection.swift
+// ProjectName: Quiz App
+//
+// Created by MD ABIR HOSSAIN on 11/8/24 at 1:51 AM
+// Website: https://mdabirhossain.com/
+//
+
+
+import Foundation
+
+extension Collection {
+    var isNotEmpty: Bool {
+        isEmpty == false
+    }
+}
