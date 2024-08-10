15b4e397f2d95e82b95b97c21331cdeb913af56a
Md Abir Hossain <hossain.mdabir@gmail.com>
2024-08-10 14:52:24 +0600

    Project created.

---
+++ Quiz App/Quiz_AppApp.swift
@@ -0,0 +1,20 @@
+//
+// 
+// FileName: Quiz_AppApp.swift
+// ProjectName: Quiz App
+//
+// Created by MD ABIR HOSSAIN on 10-08-2024 at 2:52 PM
+// Website: https://mdabirhossain.com/
+//
+
+
+import SwiftUI
+
+@main
+struct Quiz_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
