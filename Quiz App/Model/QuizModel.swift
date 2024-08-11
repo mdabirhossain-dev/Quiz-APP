73781f7645c530d607309e8700a8bf717d6849f1
Md Abir Hossain <mdabirhossain.dev@gmail.com>
2024-08-11 16:21:57 +0600

    QuizModel added.

--- Quiz App/Model/QuizModel.swift
+++ Quiz App/Model/QuizModel.swift
@@ -11,5 +11,30 @@
 import Foundation
 
 struct QuizModel: Codable {
-    
+    let questions: [Question]?
+}
+
+struct Question: Codable {
+    let question: String?
+    let answers: Answers?
+    let questionImageURL: String?
+    let correctAnswer: String?
+    let score: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case question, answers
+        case questionImageURL = "questionImageUrl"
+        case correctAnswer, score
+    }
+}
+
+struct Answers: Codable {
+    let a, b, c, d: String?
+
+    enum CodingKeys: String, CodingKey {
+        case a = "A"
+        case b = "B"
+        case c = "C"
+        case d = "D"
+    }
 }
